0f73f75945ed4df06a3403287648da177fee7313
ChavezCode <jch1248@wgu.edu>
2024-09-09 17:41:07 -0700

    Task E Complete

--- README.md
+++ README.md
@@ -16,4 +16,15 @@ D.  Add an “About” page to the application to describe your chosen customer�
     Changes on About.html: Title changed to About JC's. Header changed to About JC's Auto. A Navbar was added and link was added to go back to the mainscreen.
                            Information was added to the h3 and h5 headers about JC's auto. Paragraphs were added about the history and a quote from JC.
     Changes on AboutController: Added a controller for the About page to create mapping to get a http request to view the about page.
+
+E.  Add a sample inventory appropriate for your chosen store to the application. You should have five parts and five products in your sample inventory and should not overwrite existing data in the database.
     
+    File Name: BootStrapData.java & application.properties
+    Line Number where changes were made:
+        On BootStrapData.java: 45 - 102
+        On application.properties: 6
+    Changes made on application.properties: Changed the name of the database url so that a new database is retrieved from the model, with the data of the inventory that was created in the BootStrapData class.
+    Chaneges made to BootStrapData.java: At line 45 and 94, an if statement was created to prevent the inventory from duplicating itself everytime it was uploaded.
+                                        The Inventory will only load up again once all the sample parts or products reach 0.
+                                        Lines 47 - 90 use the set methods from the outsource part object extended from the part class, to add part information for the sample inventory.
+                                        Lines 96 - 119 use the product contructor to initialize new product objects. 
--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -1,8 +1,10 @@
 package com.example.demo.bootstrap;
 
+import com.example.demo.domain.InhousePart;
 import com.example.demo.domain.OutsourcedPart;
 import com.example.demo.domain.Part;
 import com.example.demo.domain.Product;
+import com.example.demo.repositories.InhousePartRepository;
 import com.example.demo.repositories.OutsourcedPartRepository;
 import com.example.demo.repositories.PartRepository;
 import com.example.demo.repositories.ProductRepository;
@@ -39,33 +41,91 @@ public class BootStrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-       /*
-        OutsourcedPart o= new OutsourcedPart();
-        o.setCompanyName("Western Governors University");
-        o.setName("out test");
-        o.setInv(5);
-        o.setPrice(20.0);
-        o.setId(100L);
-        outsourcedPartRepository.save(o);
-        OutsourcedPart thePart=null;
-        List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
-        for(OutsourcedPart part:outsourcedParts){
-            if(part.getName().equals("out test"))thePart=part;
+        // an if statement to prevent this inventory from duplicating each time the program is ran.
+        if (outsourcedPartRepository.count() == 0) {
+
+            //creating an object Oil Filter from the OutsourcedPart class
+            OutsourcedPart oilFilter = new OutsourcedPart();
+            oilFilter.setName("Oil Filter");
+            oilFilter.setInv(15);
+            oilFilter.setPrice(11.99);
+            oilFilter.setId(021354);
+            //saving the part to the Outsourced Part Repository
+            outsourcedPartRepository.save(oilFilter);
+
+            //creating an object Radiator Hose from the OutsourcedPart class
+            OutsourcedPart radiatorHose = new OutsourcedPart();
+            radiatorHose.setName("Radiator Hose");
+            radiatorHose.setInv(20);
+            radiatorHose.setPrice(20.50);
+            radiatorHose.setId(021355);
+            //saving the part to the Outsourced Part Repository
+            outsourcedPartRepository.save(radiatorHose);
+
+            //creating an object Water Pump from the OutsourcedPart class
+            OutsourcedPart waterPump = new OutsourcedPart();
+            waterPump.setName("Water Pump");
+            waterPump.setInv(20);
+            waterPump.setPrice(20.50);
+            waterPump.setId(021356);
+            //saving the part to the Outsourced Part Repository
+            outsourcedPartRepository.save(waterPump);
+
+            //creating an object Radiator from the OutsourcedPart class
+            OutsourcedPart radiator = new OutsourcedPart();
+            radiator.setName("Radiator");
+            radiator.setInv(25);
+            radiator.setPrice(79.99);
+            radiator.setId(021357);
+            //saving the part to the Outsourced Part Repository
+            outsourcedPartRepository.save(radiator);
+
+            //creating an object Drain Plug from the OutsourcedPart class
+            OutsourcedPart drainPlug= new OutsourcedPart();
+            drainPlug.setName("Drain Plug");
+            drainPlug.setInv(20);
+            drainPlug.setPrice(20.50);
+            drainPlug.setId(021353);
+            //saving the part to the Outsourced Part Repository
+            outsourcedPartRepository.save(drainPlug);
+
         }
 
-        System.out.println(thePart.getCompanyName());
-        */
+        if (productRepository.count() == 0) {
+
+            //creating an object Turbo Kit from the Product class
+            Product turboKit = new Product(1324, "Turbo Kit", 350.00, 5);
+            //Saving the product to the product repository
+            productRepository.save(turboKit);
+
+            //creating an object coolant kit from the product class
+            Product coolantKit = new Product(1332, "Coolant Kit", 180.50, 6);
+            //saving the product to the product repository
+            productRepository.save(coolantKit);
+
+            //creating an object tool set from the product class
+            Product toolSet  = new Product(1337, "Tool Set", 25.00, 30);
+            //saving the product to the product repository
+            productRepository.save(toolSet);
+
+            //creating an object cleaning kit to the product class
+            Product cleaningKit = new Product(1338, "Cleaning Kit", 25.00, 20);
+            //saving the product to the product repository
+            productRepository.save(cleaningKit);
+
+            //creating an object break set from the product class
+            Product breakSet = new Product(1339, "Break Set", 25.00, 20);
+            //saving the product to the product repository
+            productRepository.save(breakSet);
+
+
+        }
+
         List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
         for(OutsourcedPart part:outsourcedParts){
             System.out.println(part.getName()+" "+part.getCompanyName());
         }
 
-        /*
-        Product bicycle= new Product("bicycle",100.0,15);
-        Product unicycle= new Product("unicycle",100.0,15);
-        productRepository.save(bicycle);
-        productRepository.save(unicycle);
-        */
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Products"+productRepository.count());
